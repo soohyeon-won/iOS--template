c673699ab302fb14c0f0df40784091b60cbac675
soohyeon.won <ppkong22@hanmail.net>
2023-12-17 19:42:41 +0900

    [project] spm 추가

---
+++ Project/Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by won soohyeon on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
