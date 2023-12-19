c3acd38711d1dd736902c38649e290132f3b839c
soohyeon.won <ppkong22@hanmail.net>
2023-12-19 21:02:02 +0900

    [spm] spm package.swift로 관리 파일 변경

---
+++ Project/Project/Sources/Network/API.swift
@@ -0,0 +1,42 @@
+//
+//  API.swift
+//  Project
+//
+//  Created by won soohyeon on 12/18/23.
+//
+
+import Moya
+import CombineMoya
+import Foundation
+
+//enum API {
+//    case login
+//}
+//
+//extension API: TargetType {
+//    var baseURL: URL {
+//        return URL(string: Server.real.domain())!
+//    }
+//    
+//    var path: String {
+//        switch self {
+//        case .login:
+//            return "/login"
+//        }
+//    }
+//    
+//    var method: Moya.Method {
+//        switch self {
+//        case .login:
+//            return .get
+//        }
+//    }
+//    
+//    var task: Moya.Task {
+//        <#code#>
+//    }
+//    
+//    var headers: [String : String]? {
+//        <#code#>
+//    }
+//}
---
+++ Project/Project/Sources/Network/Server.swift
@@ -0,0 +1,16 @@
+//
+//  Config.swift
+//  Server
+//
+//  Created by won soohyeon on 12/19/23.
+//
+
+import Foundation
+
+enum Server {
+    case real
+    
+    func domain() -> String {
+        return ""
+    }
+}
