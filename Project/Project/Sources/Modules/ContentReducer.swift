4798c6c93f6f2daa133b75b0cda7cf45b81d51d1
soohyeon.won <ppkong22@hanmail.net>
2024-01-10 23:38:46 +0900

    [Architecture] add base TCA

---
+++ Project/Project/Sources/Modules/ContentReducer.swift
@@ -0,0 +1,21 @@
+//
+//  ContentReducer.swift
+//  Project
+//
+//  Created by won soohyeon on 1/10/24.
+//
+
+import Foundation
+
+struct ContentReducer {
+    
+    enum Action {
+        case viewWillAppear
+    }
+    
+    struct State {
+        var testCase: Bool = true
+    }
+    
+    
+}
