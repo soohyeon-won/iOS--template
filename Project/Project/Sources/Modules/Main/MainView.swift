5fe1162ea3e8252172716facc2bae9bb7f6b5a64
soohyeon.won <ppkong22@hanmail.net>
2024-01-21 19:22:59 +0900

    [Router] main to subView

--- Project/Project/Sources/Modules/Main/MainView.swift
+++ Project/Project/Sources/Modules/Main/MainView.swift
@@ -14,18 +14,33 @@ struct MainView: View {
     
     var body: some View {
         WithViewStore(store, observe: {$0}) { viewStore in
-            VStack {
-                Button(action: {
-                    viewStore.send(.tapBtn)
-                }, label: {
-                    Text("Toggle Button")
-                })
-                
-                Text("Toggle Button isComplete: \(String(describing: viewStore.state.isComplete))")
-                
-                TextField("텍스트 작성: ", text: viewStore.$description)
-                
-                Text("입력된 데이터: \(viewStore.description)")
+            NavigationView {
+                VStack {
+                    Button(action: {
+                        viewStore.send(.tapBtn)
+                    }, label: {
+                        Text("Toggle Button")
+                    })
+                    
+                    Text("Toggle Button isComplete: \(String(describing: viewStore.state.isComplete))")
+                    
+                    TextField("텍스트 작성: ", text: viewStore.$description)
+                    
+                    Text("입력된 데이터: \(viewStore.description)")
+                    
+                    NavigationLink(destination: SubView(store: store)) {
+                        
+                        VStack {
+                            Text("Subview 진입점")
+                                .padding([.leading, .trailing], 16)
+                                .padding([.top, .bottom], 8)
+                                .background(RoundedRectangle(cornerRadius: 10).strokeBorder(lineWidth: 1))
+                        }
+                    }
+                }
+                .navigationBarBackButtonHidden(true)
+                .navigationBarHidden(true)
+                .navigationTitle("")
             }
         }
     }
